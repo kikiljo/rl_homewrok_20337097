d42b056d53f4d857300600c0fa16dd77a2498acb
kikiljo <73957281+kikiljo@users.noreply.github.com>
2023-03-07 16:22:37 +0800

    Add files via upload

---
+++ agents/__init__.py
@@ -0,0 +1,6 @@
+#from .BaseAgent import BaseAgent
+#from .VanillaDQN import VanillaDQN
+from .DQN import DQN
+from .DDQN import DDQN
+from .MaxminDQN import MaxminDQN
+from .AveragedDQN import AveragedDQN
